94add12814a9f4c6ddcf9c96884c4cbeabd4484e
Sahil Agarwal - sahil_agarwal@iitb.ac.in <sahilag@emsys1.ee.iitb.ac.in>
2015-09-04 16:15:48 +0530

    last commit at iitb!

--- incSat/obs1.sh
+++ incSat/obs1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-for i in *.cnf; do 
-	for j in 1 2 3 4 5 6 7 8 9 10 
+for i in /home/users/sahilag/DDP/npsat/v1/sc14-app/*.cnf; do 
+	for j in 6 10 
 	do 
 		(time ./part1.o $i -nassums=$j > performance/main/$(basename $i .cnf).n_ass$j.sol1) \
 			&> performance/main/$(basename $i .cnf).n_ass$j.time1
 			
-		(time timeout 100 ./part2.o $i $j> performance/main/$(basename $i .cnf).n_ass$j.sol2) \
+		(time timeout 1000 ./part2.o $i $j> performance/main/$(basename $i .cnf).n_ass$j.sol2) \
 			&> performance/main/$(basename $i .cnf).n_ass$j.time2
 		done
 	done
